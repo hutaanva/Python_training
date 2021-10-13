cd674cc4644fbbcd3cf2095c754179611ad9539e
hutaanva <tamas.varga@lego.com>
2021-10-13 16:28:15 +0200

    Eslő python programom

---
+++ karácsonyfa.py
@@ -0,0 +1,10 @@
+stars=1
+while stars < 7:
+        #print("*" * stars)
+        line = ""
+        number = 1
+        while number < stars:
+            line += "*"
+            number += 1
+        print("line")
+        stars = stars + 1
